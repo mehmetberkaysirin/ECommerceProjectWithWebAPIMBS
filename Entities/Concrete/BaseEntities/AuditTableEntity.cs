915504722af7a8e4fa6df164515a3d646f03f66a
Mehmet Berkay ŞİRİN <mehmetberkaysirin@aof.anadolu.edu.tr>
2021-11-18 21:54:55 +0300

    Last Version

---
+++ Entities/Concrete/BaseEntities/AuditTableEntity.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditTableEntity : BaseEntity, ICreatedEntity,IUpdateEntity
+    {
+        public int CreatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? UpdateUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Entities/Enums/Genders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Enums
+{
+    public enum Genders
+    {
+        /// <summary>
+        ///     
+        /// </summary>
+        Famele=0,
+        Male=1
+    }
+}
