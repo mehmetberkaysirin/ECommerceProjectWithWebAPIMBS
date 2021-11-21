66c7481352fccd5d02d831dfe0e1c994de7044bf
Mehmet Berkay ŞİRİN <mehmetberkaysirin@aof.anadolu.edu.tr>
2021-11-21 22:25:19 +0300

    Bussines Added

---
+++ Bussines/Abstract/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Dtos.UserDtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bussines.Abstract
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+        Task<UserDto> GetByIdAsync(int id);
+        Task<UserDto> AddAsync(UserAddDto userAddDto);
+        Task<UserUpdateDto> UpdateAsync(UserUpdateDto userUpdateDto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
--- Entities/Dtos/UserDtos/UserDetailDto.cs
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -9,12 +9,11 @@ namespace Entities.Dtos.UserDtos
 {
     public class UserDetailDto:IDto
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
-
-        public string Password { get; set; }
 
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
